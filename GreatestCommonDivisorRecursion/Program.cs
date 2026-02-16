6237b9df1d2dbd15339f533daa8999a4c5dec5e7
MosesMnz <39951731+MosesMNZ@users.noreply.github.com>
2026-02-16 03:59:44 -0700

    Create a Utility class and add logic to recursively calculate the greatest common divisor

---
+++ GreatestCommonDivisorRecursion/Program.cs
@@ -0,0 +1,54 @@
+﻿/*
+ * Moses Muamba-Nzambi
+ * CST - 250
+ * 02/15/2026
+ * Greatest Common Divisor Recursion
+ * Activity 3
+ */
+
+using System;
+
+class Program
+{
+    // ------------------------------------------------------------
+    // Start of the Main Method
+    // ------------------------------------------------------------
+
+    // (Main method content not shown in images)
+
+    // ------------------------------------------------------------
+    // End of the Main Method
+    // ------------------------------------------------------------
+}
+
+// ------------------------------------------------------------
+// Start of the Utility Class
+// ------------------------------------------------------------
+public class Utility
+{
+    /// <summary>
+    /// Calculate the GCD using recursion
+    /// </summary>
+    /// <param name="num1"></param>
+    /// <param name="num2"></param>
+    /// <returns></returns>
+    internal static int GreatestCommonDivisor(int num1, int num2)
+    {
+        // Declare and initialize
+        int remainder = 0;
+
+        // Base case: num2 is 0
+        if (num1 == 0 || num2 == 0)
+        {
+            return num1;
+        }
+        else
+        {
+            remainder = num1 % num2;
+
+            Console.WriteLine($"Not yet. The remainder of {num1} and {num2} is {remainder}");
+
+            return GreatestCommonDivisor(num2, remainder);
+        }
+    }
+}
