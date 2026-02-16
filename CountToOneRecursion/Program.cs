a1516a54906104f8bc236df4378c83e246a768b7
MosesMnz <39951731+MosesMNZ@users.noreply.github.com>
2026-02-16 03:13:52 -0700

    Add functionality for the CountToOne recursive method

---
+++ CountToOneRecursion/Program.cs
@@ -0,0 +1,78 @@
+﻿/*
+ * Moses Muamba-Nzambi
+ * CST - 250
+ * 02/16/2026
+ * Count To One Recursion
+ * Activity 3
+ */
+
+using System;
+
+class Program
+{
+    // ------------------------------------------------------------
+    // Start of the Main Method
+    // ------------------------------------------------------------
+    static void Main(string[] args)
+    {
+        // Declare and initialize
+        int choice = 0, result = 0;
+        string input = "";
+
+        // Prompt the user for a number
+        Console.Write("Enter a positive number: ");
+        input = Console.ReadLine();
+
+        // Validate input
+        while (!int.TryParse(input, out choice) || choice <= 0)
+        {
+            Console.WriteLine("Invalid number");
+            Console.Write("Enter a positive number: ");
+            input = Console.ReadLine();
+        }
+
+        // Call the CountToOne function
+        result = Utility.CountToOne(choice);
+        Console.WriteLine($"The end number is {result}");
+    }
+    // ------------------------------------------------------------
+    // End of the Main Method
+    // ------------------------------------------------------------
+}
+
+// ------------------------------------------------------------
+// Start of the Utility class
+// ------------------------------------------------------------
+static class Utility
+{
+    /// <summary>
+    /// Count to one using recursion
+    /// </summary>
+    /// <param name="num"></param>
+    /// <returns></returns>
+    internal static int CountToOne(int num)
+    {
+        // Print out the current number
+        Console.WriteLine($"The current number is {num}");
+
+        // Check if the number is 1
+        if (num == 1)
+        {
+            return 1;
+        }
+        else
+        {
+            // Check if the number is even
+            if ((num % 2) == 0)
+            {
+                Console.WriteLine("The number is even. Divide by 2");
+                return CountToOne(num / 2);
+            }
+            else
+            {
+                Console.WriteLine("The number is odd. Add 1");
+                return CountToOne(num + 1);
+            }
+        }
+    }
+}
